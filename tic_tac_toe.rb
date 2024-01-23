dca354c61a5733548caa10e963e7c9716f181ad7
jtchap27 <josh.chap2@gmail.com>
2024-01-23 05:12:57 -0500

    Finalize basic game play
    A basic single game works. Now working on code to realize when a game has finished.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -17,65 +17,69 @@ end
 =end
 
 class Game
-  attr_accessor :player_1, :player_2
+  attr_accessor :player_1_name, :player_2_name
 
-  @@board_hash = {
-    a1: "-",
-    a2: "-",
-    a3: "-",
-    b1: "-",
-    b2: "X",
-    b3: "-",
-    c1: "-",
-    c2: "-",
-    c3: "-",
-  }
-
-  def initialize(player_1, player_2)
-    @player_1 = player_1
-    @player_2 = player_2
+  def initialize
+    puts "Please enter a name for player 1:"
+    @player_1_name = gets.chomp
+    puts "Hello #{@player_1_name}, welcome to tic-tac-toe!"
+    puts "Please enter a name for player 2:"
+    @player_2_name = gets.chomp
+    puts "Hello #{@player_2_name}, welcome to tic-tac-toe against #{@player_1_name}!"
+    @board_hash = { a1: "-", a2: "-", a3: "-", b1: "-", b2: "-", b3: "-", c1: "-", c2: "-", c3: "-" }
+    display_board
   end
 
+  # @winning_board_hash_array = [[@board_hash[a1]],
 
+  def display_board
+    puts "\nGAME BOARD\n\s\s1 2 3\nA #{@board_hash[:a1]} #{@board_hash[:a2]} #{@board_hash[:a3]}\nB #{@board_hash[:b1]} #{@board_hash[:b2]} #{@board_hash[:b3]}\nC #{@board_hash[:c1]} #{@board_hash[:c2]} #{@board_hash[:c3]}\n\s"
+  end
+
+  def change_spot_on_board_X(sym)
+    if @board_hash[sym] == "-"
+      @board_hash[sym] = "X"
+    else
+      puts "\n\t\s\s\s--INVALID INPUT--\n\tPlease pick an empty spot"
+      self.display_board
+      self.player_1_move
+    end
+  end
+
+  def change_spot_on_board_O(sym)
+    if @board_hash[sym] == "-"
+      @board_hash[sym] = "O"
+    else
+      puts "\n\t\s\s\s--INVALID INPUT--\n\tPlease pick an empty spot"
+      self.display_board
+      self.player_2_move
+    end
+  end
+
+  def player_1_move
+    puts "#{self.player_1_name}: please enter the spot to make your next X like so: \"B3\""
+    spot = gets.chomp.downcase.to_sym
+    self.change_spot_on_board_X(spot)
+    self.display_board
+    self.player_2_move
+  end
+
+  def player_2_move
+    puts "#{self.player_2_name}: please enter the spot to make your next O like so: \"B3\""
+    spot = gets.chomp.downcase.to_sym
+    self.change_spot_on_board_O(spot)
+    self.display_board
+    self.player_1_move
+  end
 
 end
-
-
-
-def player_1_move
-  puts "Player 1: please enter the spot to make your next X like so: \"B3\""
-  yx = gets.chomp.downcase.to_sym
-  p yx
-  board_hash[yx] = "X"
-  puts board
-  player_2_move(yx)
-end
-
-def player_2_move
-  puts "Player 2: please enter the spot to make your next O like so: \"B3\""
-  yx = gets.chomp.downcase
-  board_hash[:yx] = "O"
-  puts board
-  player_1_move(yx)
-end
-
-
-board = "\nGAME BOARD\n\s\s1 2 3\nA #{board_hash[:a1]} #{board_hash[:a2]} #{board_hash[:a3]}\nB #{board_hash[:b1]} #{board_hash[:b2]} #{board_hash[:b3]}\nC #{board_hash[:c1]} #{board_hash[:c2]} #{board_hash[:c3]}\n\s"
-
-
-
-puts board
-player_1_move
 
 =begin
-player_1 should be a class
-player_2 should be a class
-
-methods for picking a spot for X or O
-
-make most methods private or protected so that a player can't access another player's methods
-
-board display of arrays? Use print to make array look horizontal
-board should be a class variable
-
+class Player < Game
+  def initialize
+  end
+end
 =end
+
+current_game = Game.new
+current_game.player_1_move
