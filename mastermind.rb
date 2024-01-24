4d5242ecc8755c52993f049827e99f0a097f1452
jtchap27 <josh.chap2@gmail.com>
2024-01-24 16:44:26 -0500

    Add guess feedback function and randomize computer setting of code to break
    The game has the above added functionality.

--- mastermind.rb
+++ mastermind.rb
@@ -12,14 +12,16 @@ stop game when array of === spots is equal to the computer code array
 =end
 
 class Game
+  attr_reader :code_breaker
   def initialize
     code_master = ComputerPlayer.new()
     p code_master.name
     puts "Please enter a name for the human player:"
     code_breaker = HumanPlayer.new(gets.chomp)
-    p code_breaker.name
-    puts "Please enter four colors in consecutive order:"
-    @code_to_break_array = [gets.chomp, gets.chomp, gets.chomp, gets.chomp]
+    @code_breaker = code_breaker.name
+    p @code_breaker
+    @array_of_colors = ["blue", "green", "white", "red", "black", "purple"]
+    @code_to_break_array = @array_of_colors.sample(4)
     @guesses_array = [
       ["---", "---", "---", "---"],
       ["---", "---", "---", "---"],
@@ -30,11 +32,42 @@ class Game
       ["---", "---", "---", "---"],
       ["---", "---", "---", "---"],
     ]
+    @feedback_array = []
+    display_game_board
+  end
+
+  def make_guess
+    i = @guesses_array.length - 1
+    while i >= 0 do
+      k = 0
+      while k <= 3 do
+        puts "#{@code_breaker}, please make a guess:"
+        @guesses_array[i][k] = gets.chomp
+        k += 1
+      end
+      feedback_array_to_push = []
+      j = 0
+      while j <= 3 do
+        if @guesses_array[i][j] == @code_to_break_array[j]
+          feedback_array_to_push.push(" C ")
+        elsif @code_to_break_array.include?(@guesses_array[i][j])
+          feedback_array_to_push.push(" c ")
+        end
+        j += 1
+      end
+      @feedback_array.push(feedback_array_to_push.shuffle)
+      display_game_board
+      i -= 1
+    end
+  end
+  
+  def play_a_game
+    make_guess
     display_game_board
   end
 
   def display_game_board
-    puts "\s\s\s\s\s\s\sCURRENT GAME BOARD\n\n\s\s\sCode #{@code_to_break_array}\n\nGuess 1 #{@guesses_array[0]}\nGuess 2 #{@guesses_array[1]}\nGuess 3 #{@guesses_array[2]}\nGuess 4 #{@guesses_array[3]}\nGuess 5 #{@guesses_array[4]}\nGuess 6 #{@guesses_array[4]}\nGuess 7 #{@guesses_array[6]}\nGuess 8 #{@guesses_array[7]}"
+    puts "\s\s\s\s\s\s\sCURRENT GAME BOARD\n\n\s\s\sCode #{@code_to_break_array}\n\nGuess 1 #{@guesses_array[0]}\nGuess 2 #{@guesses_array[1]}\nGuess 3 #{@guesses_array[2]}\nGuess 4 #{@guesses_array[3]}\nGuess 5 #{@guesses_array[4]}\nGuess 6 #{@guesses_array[4]}\nGuess 7 #{@guesses_array[6]}\nGuess 8 #{@guesses_array[7]}\n\nFeedback\n#{@feedback_array}"
   end
 
   def display_array_of_guesses
@@ -57,4 +90,5 @@ class HumanPlayer < Game
   end
 end
 
-play_game = Game.new
+mm_round = Game.new
+mm_round.play_a_game
